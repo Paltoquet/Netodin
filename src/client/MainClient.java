ae1ba6714cbb13751434d198cb10cf995f667d8d
Florian Pradines <florian.pradines@gmail.com>
2015-11-10 15:03:11 +0100

    a

---
+++ src/client/MainClient.java
@@ -0,0 +1,16 @@
+package client;
+
+import exceptions.ConnectionException;
+
+public class MainClient {
+
+    public static void main(String [] args){
+        try {
+            Client client = new Client("127.0.0.1", 1337);
+            client.run();
+        } catch (ConnectionException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
