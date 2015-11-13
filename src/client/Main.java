9ce60d9631fedd951638ed2997f7996253f801b8
Thibault OBER <thibault.ober@gmail.com>
2015-11-13 14:08:17 +0100

    default parameters main

--- src/client/Main.java
+++ src/client/Main.java
@@ -6,7 +6,18 @@ public class Main
 {
     public static void main(String [] args){
         try {
-            Client client = new Client(args[0], Integer.parseInt(args[1]));
+            String host = "127.0.0.1";
+            int port = 1337;
+
+            if (args.length > 1) {
+                host = args[0];
+            }
+
+            if (args.length > 2) {
+                port = Integer.parseInt(args[1]);
+            }
+
+            Client client = new Client(host, port);
             client.run();
         } catch (ConnectionException e) {
             System.err.println(e.getMessage());
